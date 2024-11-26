3e06b77b9f8b40c1fc2d7b6774f7331445263390
Katarinawallenhoff <katarina.wallenhoff@outlook.com>
2024-11-26 14:07:41 +0100

    ShowCity function
    If and else to write out what will stand in the h2 element. CurrentCity will get the value of another function that takes the userInput and checks if it's the same in the array.  Else will be written in the H2 if findCity gets value "undefined".

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
 let divContainer = document.getElementById("cities");
 let userInput = prompt("Skriv in en stad");
 let h2Element = document.querySelector("h2");
+let currentCity = 0;
+
+//Start of all functions
+
+
+function showCity() {
+    currentCity = findCity(userInput);
+ 
+ 
+    if (currentCity) {
+        h2Element.textContent = currentCity.name + " (" + currentCity.country + ")";
+        document.title = currentCity.name;
+    } else {
+        h2Element.textContent = userInput + " finns inte i databasen.";
+        document.title = "not found";
+    }
+ }
+ 
